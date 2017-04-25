0ba47324c24cb6d95e8a7baefbf35fa6187ac8de
Diogo Duarte <deduarte@ua.pt>
2017-04-26 00:43:45 +0100

    Fragment Pick Exercise

--- Muscle/app/src/main/java/g11/muscle/HomeActivity.java
+++ Muscle/app/src/main/java/g11/muscle/HomeActivity.java
@@ -1,15 +1,23 @@
 package g11.muscle;
 
 import android.os.Bundle;
+
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+
 import android.view.MenuItem;
+
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import android.support.v4.app.FragmentManager;
+
+import android.net.Uri;
+
+public class HomeActivity extends AppCompatActivity implements PickExerciseFragment.OnFragmentInteractionListener{
 
     private TextView mTextMessage;
+    private static String email;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,6 +30,7 @@ public class HomeActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_exerciseList:
                     mTextMessage.setText(R.string.title_exerciseList);
+                    switchToFragmentPickExercise();
                     return true;
                 case R.id.navigation_exerciseHistory:
                     mTextMessage.setText(R.string.title_exerciseList);
@@ -39,12 +48,25 @@ public class HomeActivity extends AppCompatActivity {
     };
 
     @Override
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty - Used for interaction between fragments
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        email = getIntent().getStringExtra("email");
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    // Method used to switch to PickExercise Fragment
+    public void switchToFragmentPickExercise() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.content, new PickExerciseFragment()).commit();
     }
 }
