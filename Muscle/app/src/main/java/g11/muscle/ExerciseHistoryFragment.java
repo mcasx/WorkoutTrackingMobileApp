534567453ed45f99b88eeeed434f0bbfcad6fcdd
Diogo Duarte <deduarte@ua.pt>
2017-04-27 20:01:05 +0100

    Clean imports - For Merge

--- Muscle/app/src/main/java/g11/muscle/ExerciseHistoryFragment.java
+++ Muscle/app/src/main/java/g11/muscle/ExerciseHistoryFragment.java
@@ -12,15 +12,11 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ListView;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
