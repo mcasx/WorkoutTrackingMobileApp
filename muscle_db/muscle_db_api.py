f77f134ec1aa9ce2e7de0dd97597cb77dfe4af24
Manuel Xarez <manuelxarez@ua.pt>
2017-05-02 11:57:04 +0100

    Fixed some bugs and added recommended follows

--- muscle_db/muscle_db_api.py
+++ muscle_db/muscle_db_api.py
@@ -1435,6 +1435,34 @@ def get_sets_of_exercise_history():
 	#	return str(e)
     
     
+@app.route('/get_recommended_follows', methods=['POST'])
+def get_recommended_follows():
+	try:
+		email = request.form['email_user']
+		limit = int(request.form['limit'])
+		conn = MySQLdb.connect(host='localhost', user='muscle', password='some_pass', database='muscle')
+		c = conn.cursor(MySQLdb.cursors.DictCursor)
+		
+		c.execute('USE muscle2')
+		c.execute("""
+				SELECT Following, count(Following) AS count
+				FROM FOLLOWERS
+				JOIN (SELECT Following
+					FROM FOLLOWERS
+					WHERE User_email = %s) AS t
+				ON FOLLOWERS.User_email = t.Following
+				GROUP BY Following
+				ORDER BY count DESC
+				LIMIT %s
+				""", [email, limit])
+
+		r = c.fetchall()
+		c.close()
+		conn.close()
+		return jsonify(r) 
+
+	except Exception as e:
+		return str(e)
 
 if __name__ == '__main__':
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
