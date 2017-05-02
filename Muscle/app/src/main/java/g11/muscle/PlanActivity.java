286f11787f3a08c7cf95e22b7445a280aac72cff
Silvério Matos Pereira <p.m.s@ua.pt>
2017-05-02 18:37:29 +0100

    PlanActivity fo recommended plans ;)

---
+++ Muscle/app/src/main/java/g11/muscle/PlanActivity.java
@@ -0,0 +1,13 @@
+package g11.muscle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+    }
+}
