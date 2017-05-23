4ca34c62486dd9cd5dcf4cdcdaf3fa9b8c6b6700
Tiago Madeira <negativespade@gmail.com>
2017-05-23 14:44:50 +0100

    bnce intepla

---
+++ Muscle/app/src/main/java/g11/muscle/BounceInterpolator.java
@@ -0,0 +1,16 @@
+package g11.muscle;
+
+class BounceInterpolator implements android.view.animation.Interpolator {
+    double mAmplitude = 1;
+    double mFrequency = 10;
+
+    BounceInterpolator(double amplitude, double frequency) {
+        mAmplitude = amplitude;
+        mFrequency = frequency;
+    }
+
+    public float getInterpolation(float time) {
+        return (float) (-1 * Math.pow(Math.E, -time/ mAmplitude) *
+                Math.cos(mFrequency * time) + 1);
+    }
+}
