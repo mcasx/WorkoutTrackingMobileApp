0a537c16b2faed0561384b978b71f3fd663d4a6e
Diogo Duarte <deduarte@ua.pt>
2017-05-30 01:41:44 +0100

    Graph probably working (needs Label and Colors)

--- Muscle/app/src/main/java/g11/muscle/Fragments/HomeFragment.java
+++ Muscle/app/src/main/java/g11/muscle/Fragments/HomeFragment.java
@@ -1,11 +1,14 @@
 package g11.muscle.Fragments;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -27,12 +30,31 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Array;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -40,6 +62,7 @@ import java.util.Map;
 
 import g11.muscle.Classes.MuscleProgressItem;
 import g11.muscle.DB.DBConnect;
+import g11.muscle.DB.MuscleDbContract;
 import g11.muscle.ExerciseActivity;
 import g11.muscle.GroupExercisesActivity;
 import g11.muscle.PlanActivity;
@@ -57,7 +80,7 @@ import static g11.muscle.R.layout.muscle_progress_view;
  * Use the {@link HomeFragment# newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnChartValueSelectedListener {
     private static final String TAG = "HomeFragment";
 
     private OnFragmentInteractionListener mListener;
@@ -72,6 +95,10 @@ public class HomeFragment extends Fragment {
     private View fView;
     private ProgressBar progressBar;
     private GridView muscle_groupView;
+    private HorizontalBarChart bChart;
+
+    // Chart font
+    private Typeface mTfLight;
 
     private ArrayList<MuscleProgressItem> list;
 
@@ -101,6 +128,10 @@ public class HomeFragment extends Fragment {
         progressBar.setVisibility(View.VISIBLE);
         recList = (ListView) fView.findViewById(R.id.home_rec_list);
 
+        bChart = (HorizontalBarChart) fView.findViewById(R.id.home_bar_chart);
+        // Font for charts text
+        mTfLight = Typeface.createFromAsset(getActivity().getAssets(), "fonts/OpenSans-Light.ttf");
+
         spinner = (Spinner) fView.findViewById(R.id.home_spinner);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
@@ -116,6 +147,7 @@ public class HomeFragment extends Fragment {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+
         getMuscleProgress();
         // Inflate the layout for this fragment
         return fView;
@@ -168,6 +200,7 @@ public class HomeFragment extends Fragment {
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
+
                         list = new ArrayList<>();
                         Iterator<String> iter = response.keys();
                         while (iter.hasNext()) {
@@ -183,6 +216,9 @@ public class HomeFragment extends Fragment {
                         // Define the groupView adapter
 
                         progressBar.setVisibility(View.GONE);
+
+                        // set bar chart
+                        barChartSetup();
                     }
 
                 },
@@ -208,6 +244,104 @@ public class HomeFragment extends Fragment {
         VolleyProvider.getInstance(getActivity()).addRequest(StrHistReq);
     }
 
+    private void barChartSetup(){
+
+        final String [] MuscleGroups = new String[list.size()];
+        ArrayList<BarEntry> yValues = new ArrayList<>();
+
+        float chartMax = -101f;
+        float chartMin = 1000f;
+
+        for(int i = 0, k = 5;i<list.size();i++, k += 5){
+            MuscleGroups[i] = list.get(i).getName();
+            float tmp_value = (float) ((list.get(i).getVar()-1)*100);
+            yValues.add(new BarEntry(k,tmp_value));
+
+            if(chartMax < tmp_value)
+                chartMax = tmp_value;
+            if(chartMin > tmp_value)
+                chartMin = tmp_value;
+        }
+
+        bChart.setOnChartValueSelectedListener(this);
+        bChart.setDrawGridBackground(false);
+        bChart.getDescription().setEnabled(false);
+
+        // scaling can now only be done on x- and y-axis separately
+        bChart.setPinchZoom(false);
+
+        bChart.setDrawBarShadow(false);
+        bChart.setDrawValueAboveBar(true);
+        bChart.setHighlightFullBarEnabled(false);
+
+
+        bChart.getAxisLeft().setEnabled(false);
+        bChart.getAxisRight().setAxisMaximum(chartMax+5f);
+        bChart.getAxisRight().setAxisMinimum(chartMin-5f);
+        bChart.getAxisRight().setDrawGridLines(false);
+        bChart.getAxisRight().setDrawZeroLine(true);
+        bChart.getAxisRight().setLabelCount(7, false);
+        bChart.getAxisRight().setTextColor(Color.WHITE);
+        bChart.getAxisRight().setTextSize(9f);
+
+        XAxis xAxis = bChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setTextSize(9f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setAxisMaximum(MuscleGroups.length*10);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setLabelCount(12);
+        xAxis.setGranularity(10f);
+
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return MuscleGroups[Math.abs((int)value) % MuscleGroups.length];
+            }
+        });
+
+        Legend l = bChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setFormSize(8f);
+        l.setTextColor(Color.WHITE);
+        l.setFormToTextSpace(4f);
+        l.setXEntrySpace(6f);
+
+        BarDataSet set = new BarDataSet(yValues, "Age Distribution");
+        set.setDrawIcons(false);
+        set.setValueTextSize(7f);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(Color.rgb(67,67,72));
+        set.setColors(new int[] {Color.rgb(67,67,72), Color.rgb(124,181,236)});
+        set.setDrawValues(false);
+        set.setLabel("Label Here");
+
+        BarData data = new BarData(set);
+        data.setBarWidth(8.5f);
+        bChart.setData(data);
+        bChart.invalidate();
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+        BarEntry entry = (BarEntry) e;
+        Log.i("VAL SELECTED",
+                "Value: " + Math.abs(entry.getYVals()[h.getStackIndex()]));
+    }
+
+    @Override
+    public void onNothingSelected() {
+        // TODO Auto-generated method stub
+        Log.i("NOTING SELECTED", "");
+    }
+    
     // Get Recommended Exercises ( ID + Name )
     private void getRecommendedExercises() {
         String url = DBConnect.serverURL + "/get_recommended_exercises";
